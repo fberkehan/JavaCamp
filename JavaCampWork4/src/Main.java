0dcfb1cb795c5419d21f067fc6ebdad3c6a68d5c
mbksoftware <83861132+mbksoftware@users.noreply.github.com>
2021-05-10 16:51:57 +0300

    Update Main.java

--- JavaCampWork4/src/Main.java
+++ JavaCampWork4/src/Main.java
@@ -8,16 +8,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Player player1 = new Player(1,"Mert Bekir", "Karademir", "00000000000", 2005);
-		Game game1 = new Game(1, "Valorant", "Online FPS Game", 0);
+		Player player1 = new Player(1,"Mert Bekir", "Karademir", "00000000000", 2005); // player1 isimli yeni bir oyuncu.
+		Game game1 = new Game(1, "Valorant", "Online FPS Game", 0); //game1 isimli yeni bir oyun
 		
-		PlayerCheckManager playerCheckManager = new PlayerCheckManager();
-		PlayerManager playerManager = new PlayerManager(playerCheckManager);
-		GameManager gameManager = new GameManager();
+		PlayerCheckManager playerCheckManager = new PlayerCheckManager(); //Kullanici kontrol sistemi için referans tanımlama.
+		PlayerManager playerManager = new PlayerManager(playerCheckManager); //Kullanici islemleri icin referans tanımlama.
+		GameManager gameManager = new GameManager(); //oyun islemleri icin referans tanimlama
 		
-		playerManager.add(player1);
+		playerManager.add(player1); //player1 isimli oyuncuyu ekleme
 		
-		gameManager.add(game1, player1);
+		gameManager.add(game1, player1); //player1 isimli oyuncuya game1'i ekleme.
 	}
 
 }
